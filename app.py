c1eb939689bbfcf616258f540f9eb778a8ff42b2
DataAnalystSHD <DataAnalystSHD@gmail.com>
2025-07-10 11:29:31 +0700

    improve

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, request, jsonify
-from pythainlp.transliterate import romanize
+from thai2rom import thai2rom
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Thai karaoke romanization service running!"
 
 @app.route('/romanize', methods=['POST'])
 def transliterate():
     data = request.get_json()
     text = data.get("text", "")
+    romanized = thai2rom(text)
+    romanized_clean = romanized.replace("-", "")  # ✅ remove dashes
     return jsonify({
-        "romanized": romanize(text)
+        "romanized": romanized_clean
     })
 
 if __name__ == '__main__':
